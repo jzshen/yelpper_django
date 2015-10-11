4605d944a8b5286e4b3b9d98999f22c8b12f98d7
jzshen <jennifershen26@gmail.com>
2015-10-10 18:31:42 -0700

    commit three

--- yelpper_django_app/urls.py
+++ yelpper_django_app/urls.py
@@ -19,4 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index')
+	#url(r'^post/request_upload$', 'views.request', name='post_request')
+    #url(r'^request/$', views.request, name='request')
 ]
--- yelpper_django_app/views.py
+++ yelpper_django_app/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
+from django.template import Context, loader, Template
 
 # Create your views here.
 
 from django.http import HttpResponse
+from yelpper_django_app.request import RequestForm
 
 def index(request):
-    return HttpResponse("test")
+	t = Template("my name is {{ my_name }}")
+	c = Context({"my_name": "test"})
+	#t = loader.get_template('template/yelpper_django_app/index.html')
+	#c = Context("")
+ 	return HttpResponse(t.render(c))
+
+def request(request):
+	if request.method == 'GET':
+		form = RequestForm()
+	elif request.method == 'POST': # If the form has been submitted...
+		form = ReqestForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+        	distance = form.cleaned_data['distance'] 
+    		prices = form.cleaned_data['prices']
+    		uberType = form.cleaned_data['sender']
+	else:
+		form = RequestForm() # An unbound form
+
+	return render(request, 'index.html', {
+        'form': form
+    })
+
+
 
